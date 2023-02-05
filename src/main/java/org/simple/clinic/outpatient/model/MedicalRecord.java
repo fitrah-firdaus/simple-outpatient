53f5dcc73bff1fa586ba413f68bf3de6a4c8345a
Fitrah Elly Firdaus <fitrah.firdaus@gmail.com>
2023-02-05 12:25:42 +0700

    Remove ToDo Generated

--- src/main/java/org/simple/clinic/outpatient/model/MedicalRecord.java
+++ src/main/java/org/simple/clinic/outpatient/model/MedicalRecord.java
@@ -136,7 +136,7 @@ public class MedicalRecord extends BaseModel implements Serializable {
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
+        
         if (!(object instanceof MedicalRecord)) {
             return false;
         }
--- src/test/java/org/simple/clinic/outpatient/OutpatientTest.java
+++ src/test/java/org/simple/clinic/outpatient/OutpatientTest.java
@@ -1,7 +1,6 @@
 package org.simple.clinic.outpatient;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -10,11 +9,6 @@ class OutpatientTest {
 	@BeforeAll
 	static void setup(){
 		System.setProperty("java.awt.headless", "false");
-	}
-
-	@Test
-	void contextLoads() {
-		
 	}
 
 }
